dd5cf16b11418950a323384f3d3c334c6bd8b638
Tony Deverill <DevTony72@gmail.com>
2016-10-30 00:05:15 +0100

    Finished Implementation of REST InterestController

--- src/main/java/com/hackmanchester/backend/controller/InterestController.java
+++ src/main/java/com/hackmanchester/backend/controller/InterestController.java
@@ -1,6 +1,9 @@
 package com.hackmanchester.backend.controller;
 
 import com.hackmanchester.backend.model.Interest;
+import com.hackmanchester.backend.service.InterestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +16,42 @@ import java.util.List;
 @RestController
 public class InterestController
 {
+    @Autowired
+    private InterestService interestService;
+
     public InterestController() {}
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/Interest", method = RequestMethod.GET)
     public List<Interest> getInterests()
     {
-        return null;
+        return interestService.getAllInterest();
     }
+
+    @RequestMapping(value = "/Interest", method = RequestMethod.POST)
+    public void addInterest(@RequestBody Interest interest)
+    {
+        if(interest == null)
+            throw new NullPointerException("Interest object passed to addInterest() in InterestController is NULL.");
+
+        interestService.insert(interest);
+    }
+
+    @RequestMapping(value = "/Interest", method = RequestMethod.PUT)
+    public void updateInterest(@RequestBody Interest interest)
+    {
+        if(interest == null)
+            throw new NullPointerException("Interest object passed to updateInterest() in InterestController is NULL.");
+
+        interestService.updateInterest(interest);
+    }
+
+    @RequestMapping(value = "/Interest", method = RequestMethod.DELETE)
+    public void deleteInterest(@RequestBody Interest interest)
+    {
+        if(interest == null)
+            throw new NullPointerException("Interest object passed to deleteInterest() in InterestController is NULL.");
+
+        interestService.deleteInterest(interest.getInterestID());
+    }
+
 }
