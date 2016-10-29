f01dd8d70d42ffc918563608af2a4b6e8a5eac5b
Tony Deverill <DevTony72@gmail.com>
2016-10-29 23:37:14 +0100

    Implemented InterestService and configured it's beans

--- src/main/java/com/hackmanchester/backend/service/InterestService.java
+++ src/main/java/com/hackmanchester/backend/service/InterestService.java
@@ -1,8 +1,17 @@
 package com.hackmanchester.backend.service;
 
+import com.hackmanchester.backend.model.Interest;
+
+import java.util.List;
+
 /**
  * Created by tony on 29/10/2016.
  */
 public interface InterestService
 {
+    int insert(Interest interest);
+    Interest getInterest(int id);
+    List<Interest> getAllInterest();
+    void deleteInterest(int id);
+    void updateInterest(Interest interest);
 }
---
+++ src/main/java/com/hackmanchester/backend/service/impl/InterestServiceImpl.java
@@ -0,0 +1,42 @@
+package com.hackmanchester.backend.service.impl;
+
+import com.hackmanchester.backend.model.Interest;
+import com.hackmanchester.backend.repo.InterestRepo;
+import com.hackmanchester.backend.service.InterestService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by tony on 29/10/2016.
+ */
+public class InterestServiceImpl implements InterestService
+{
+    @Autowired
+    private InterestRepo interestRepo;
+
+    public int insert(Interest interest)
+    {
+        return interestRepo.insert(interest);
+    }
+
+    public Interest getInterest(int id)
+    {
+        return interestRepo.getInterest(id);
+    }
+
+    public List<Interest> getAllInterest()
+    {
+        return interestRepo.getAllInterest();
+    }
+
+    public void deleteInterest(int id)
+    {
+        interestRepo.deleteInterest(id);
+    }
+
+    public void updateInterest(Interest interest)
+    {
+        interestRepo.updateInterest(interest);
+    }
+}
